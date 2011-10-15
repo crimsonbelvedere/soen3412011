e2ffe6201e48e31865fdf7d652a7d00804b907d5
illysam <illysam@dbfbf66e-fa1e-9207-fdf8-c4d726044e14>
2011-10-15 20:59:41 +0000

    database scripts fixed, still missing: Group_has_course

--- db/db_data.sql
+++ db/db_data.sql
@@ -1,21 +1,20 @@
-INSERT INTO faculty (id,name) VALUES ('0','Engineering and Computer Science');
+INSERT INTO faculty (name) VALUES ('Engineering and Computer Science');
 
-INSERT INTO department(id,name,faculty) VALUES ('0','Computer Science and Software Engineering','0');
+INSERT INTO department(name,faculty) VALUES ('Computer Science and Software Engineering','1');
 
-INSERT INTO program (id,name) VALUES ('0','BEng in Software Engineering');
+INSERT INTO program (name) VALUES ('BEng in Software Engineering');
 
-INSERT INTO Schedule_type (id, description, abbreviation) VALUES ('0', 'Lecture', 'Lect');
-INSERT INTO Schedule_type (id, description, abbreviation) VALUES ('1', 'Lab', 'Lab');
-INSERT INTO Schedule_type (id, description, abbreviation) VALUES ('2', 'Tutorial', 'Tut');
+INSERT INTO Schedule_type (description,abbreviation) VALUES ('Lecture','Lect');
+INSERT INTO Schedule_type (description,abbreviation) VALUES ('Lab','Lab');
+INSERT INTO Schedule_type (description,abbreviation) VALUES ('Tutorial','Tut');
 
-INSERT INTO course_group (id,name,program) VALUES ('0','Core Courses','0');
-INSERT INTO course_group (id,name,program) VALUES ('1','Options','0');
-INSERT INTO course_group (id,name,program) VALUES ('2','Electives','0');
-INSERT INTO course_group (id,name,program,course_group) VALUES ('3','Engineering Core','0','0');
-INSERT INTO course_group (id,name,program,course_group) VALUES ('4','Software Engineering Core','0','0');
-INSERT INTO course_group (id,name,program,course_group) VALUES ('5','Computer Science Group','0','0');
-INSERT INTO course_group (id,name,program,course_group) VALUES ('6','Basic Science Courses','0','0');
-INSERT INTO course_group (id,name,program,course_group) VALUES ('7','Computer Games (CG) Option','0','1');
-INSERT INTO course_group (id,name,program,course_group) VALUES ('8','Web Services and Applications (WSA) Option','0','1');
-INSERT INTO course_group (id,name,program,course_group) VALUES ('9','Real-Time, Embedded, and Avionics Software (REA) Option','0','1');
-
+INSERT INTO course_group (name,program) VALUES ('Core Courses','1');
+INSERT INTO course_group (name,program) VALUES ('Options','1');
+INSERT INTO course_group (name,program) VALUES ('Electives','1');
+INSERT INTO course_group (name,program,course_group) VALUES ('Engineering Core','1','1');
+INSERT INTO course_group (name,program,course_group) VALUES ('Software Engineering Core','1','1');
+INSERT INTO course_group (name,program,course_group) VALUES ('Computer Science Group','1','1');
+INSERT INTO course_group (name,program,course_group) VALUES ('Basic Science Courses','1','1');
+INSERT INTO course_group (name,program,course_group) VALUES ('Computer Games (CG) Option','1','1');
+INSERT INTO course_group (name,program,course_group) VALUES ('Web Services and Applications (WSA) Option','1','1');
+INSERT INTO course_group (name,program,course_group) VALUES ('Real-Time, Embedded, and Avionics Software (REA) Option','1','1');
